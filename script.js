e031c5044649a70c60957c02932218d868cb7067
alisonvpereira <alisonvpereira@gmail.com>
2020-06-25 17:37:18 +0800

    added carousel to index page

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+var scrollIndex = 0;
+startScroll();
+
+function startScroll() {
+  var i;
+  var scroll = document.getElementsByClassName("carousel");
+  var bullet = document.getElementsByClassName("bullet");
+  for (i = 0; i < scroll.length; i++) {
+    scroll[i].style.display = "none";  
+  }
+  scrollIndex++;
+  if (scrollIndex > scroll.length) {scrollIndex = 1}    
+  for (i = 0; i < bullet.length; i++) {
+    bullet[i].className = bullet[i].className.replace(" active", "");
+  }
+  scroll[scrollIndex-1].style.display = "block";  
+  bullet[scrollIndex-1].className += " active";
+  setTimeout(startScroll, 5000); // Change image every 5 seconds
+}
